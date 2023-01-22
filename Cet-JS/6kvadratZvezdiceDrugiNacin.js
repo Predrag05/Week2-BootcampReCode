0deaa1b1637ef9d220459cc93cfb0a4bd8b37632
Predrag05 <sim.pedja@gmail.com>
2023-01-23 00:04:19 +0100

    drugi nacin ya kvadrat sa zvezdicama

---
+++ Cet-JS/6kvadratZvezdiceDrugiNacin.js
@@ -0,0 +1,11 @@
+let N = 10
+let M = 5
+
+let pokolopac = '*'.repeat(N)
+let red = '*' + ' '.repeat(N - 2) + '*'
+
+console.log(pokolopac)
+for(let i = 0; i < M - 2; i++){
+    console.log(red)
+}
+console.log(pokolopac)
